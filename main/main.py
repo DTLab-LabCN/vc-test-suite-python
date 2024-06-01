b1bd33cddb7650b53c5bc12bde4ae68379af0acc
Artin-Biniek <artinbiniek@cmail.carleton.ca>
2024-06-01 10:32:50 -0400

    Improved on file structure
    The purpose of this commit was to improve on the current file structure. Additional changes were also made to __main__.py to have a better structure as well.

--- main/main.py
+++
@@ -1,8 +0,0 @@
-#Executing from main
-from w3c import get_version, test_data_model_v1
-import sys
-import json
-json_str=sys.argv[1] 
-vc = json.loads(json_str)
-print(get_version(vc))
-print('\n',test_data_model_v1(vc))
